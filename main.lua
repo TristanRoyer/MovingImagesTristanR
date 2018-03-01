91dd271558d07745bc23bf151d2c02e6ad4c07f3
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-03-01 17:11:09 -0500

    I added a rocketship and code for it

--- main.lua
+++ main.lua
@@ -17,27 +17,50 @@ scrollSpeed = 3
 local backgroundImage = display.newImageRect("Images/background.png",2048,1536)
 
 --character image with width and height
-local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 100)
+
+-- 2nd character image with width and height
+local rocketship = display.newImageRect("Images/rocketship.png", 200, 100)
+
+--set rocketship to be transparent
+rocketship.alpha = 0
 
 -- set the image to be transparent
 beetleship.alpha = 0
 
 -- set the initial x and y position of beetleship
-beetleship.X = 0
-beetleship.Y = display.contentHeight/3
+beetleship.x = 0
+beetleship.y = 50
+
+--set the initial x and y position of rocketship
+rocketship.x = 400
+rocketship.y = 300
+
+--change the direction where the rocketship is facing
+rocketship:scale (-1, 1) 
 
 -- Function: MoveShip
 -- Input: this function accepts an event listener
 -- Output: none
 -- Description: This function adds the scroll sp-eed to the x-value of the ship
 local function MoveShip(event)
-	-- add tge scroll speed to the x-value of the ship
-	beetleship.X = beetleship.X + scrollSpeed
-	-- change the transparency of the ship every time it moves so that is fades 
+	-- add the scroll speed to the x-value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+
+	-- change the transparency of the ship every time it moves so that it fades 
 	-- out
 	beetleship.alpha = beetleship.alpha + 0.01
+
+	-- add the scroll speed to the x-value of the Rocketship
+    rocketship.x = rocketship.x - scrollSpeed
+
+    -- change the transparency of the Rocketship every time it moves so that it fades
+    --out
+    rocketship.alpha = rocketship.alpha + 0.01
 end
 
---MoveSbip will be called over and over again
+--MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
+
+
 
