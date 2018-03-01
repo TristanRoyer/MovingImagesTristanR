236b3ad803544d755143ee10dde1079593565412
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-03-01 10:46:04 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+-- Title: MovingImages
+-- Name: Tristan Royer
+-- Course: ICS2O/3C
+-- This program displays a beetleship moving across the screen as well as another 
+--object moving in a different direction.
+
+--Testing
+print ("***Testing bettleship!")
+
+-- hide the status bar
