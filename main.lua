1f261b663783c3e00767e23a84caff441cffd3ae
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-03-05 11:03:15 -0500

    added spin

--- main.lua
+++ main.lua
@@ -11,7 +11,8 @@ print ("***Testing bettleship!")
 display.setStatusBar(display.hiddenStatusBar)
 
 -- global variables
-scrollSpeed = 3 
+scrollSpeed = 3
+
 
 -- background image with width and height 
 local backgroundImage = display.newImageRect("Images/background.png",2048,1536)
@@ -39,25 +40,44 @@ rocketship.y = 300
 --change the direction where the rocketship is facing
 rocketship:scale (-1, 1) 
 
+-- changes the direction when it reaches the end of the screen
+if  (beetleship.y > -100 ) 
+then beetleship.y = beetleship.y + scrollSpeed
+rocketship:scale(1,-1)
+end
+
+-- changes the direction when it reaches the end of the screen
+if  (rocketship.x > 0 )
+then rocketship.x = rocketship.x + scrollSpeed
+rocketship:scale (1,1)
+end
+
 -- Function: MoveShip
 -- Input: this function accepts an event listener
 -- Output: none
--- Description: This function adds the scroll sp-eed to the x-value of the ship
+-- Description: This function adds the scroll speed to the x-value of the ship
 local function MoveShip(event)
-	-- add the scroll speed to the x-value of the ship
+	-- add the scroll speed to the y-value of the ship
 	beetleship.y = beetleship.y - scrollSpeed
 
 	-- change the transparency of the ship every time it moves so that it fades 
 	-- out
 	beetleship.alpha = beetleship.alpha + 0.01
 
+	
+end
+
+-- Function: MoveShip
+-- Input: this functuon accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the y value of the ship
+local function MoveRocketShip(event)
 	-- add the scroll speed to the x-value of the Rocketship
     rocketship.x = rocketship.x - scrollSpeed/2
 
     -- change the transparency of the Rocketship every time it moves so that it fades
     --out
     rocketship.alpha = rocketship.alpha - 0.001
-end
 
 --MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
